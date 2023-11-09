8c0e1c4bfdb4ed863290867f19a7c9d0b4db3dff
drakeerv <drakeerv@outlook.com>
2023-11-09 10:24:20 -0500

    Multithread build script

--- package.py
+++ package.py
@@ -2,35 +2,64 @@
 
 import os
 import zipfile
+import threading
 
 ONLINE = True
 if ONLINE:
     import requests
-    import minify_html
-    import rcssmin
 else:
     import rjsmin
+import minify_html
+import rcssmin
+
+LOCK = threading.Lock()
+
 
 def minimize_js(js: bytes) -> bytes:
     # toptal is unbeatable at minifying js
     if ONLINE:
-        return requests.post("https://www.toptal.com/developers/javascript-minifier/api/raw", data={"input": js.decode()}).text.encode()
+        return requests.post(
+            "https://www.toptal.com/developers/javascript-minifier/api/raw",
+            data={"input": js.decode()},
+        ).text.encode()
     return rjsmin.jsmin(js)
+
 
 def minimize_css(css: bytes) -> bytes:
     # toptal is pretty much the same as rcssmin so we'll use that to save requests
     return rcssmin.cssmin(css)
 
+
 def minimize_html(html: bytes) -> bytes:
     # for some reason toptal sucks at minifying html
-    return minify_html.minify(html.decode(), minify_css=True,
-                              minify_js=True, remove_bangs=True, keep_html_and_head_opening_tags=True, do_not_minify_doctype=True).encode()
+    return minify_html.minify(
+        html.decode(),
+        minify_css=True,
+        minify_js=True,
+        remove_bangs=True,
+        keep_html_and_head_opening_tags=True,
+        do_not_minify_doctype=True,
+    ).encode()
 
-MINIFY = {
-    ".js": minimize_js,
-    ".css": minimize_css,
-    ".html": minimize_html
-}
+
+def add_file_thread(file: str, ziph: zipfile.ZipFile):
+    if os.path.isdir(file):
+        zipdir(file, ziph)
+    else:
+        if file.lower().endswith(tuple(MINIFY.keys())):
+            with open(file, "rb") as f:
+                minified_data = MINIFY[os.path.splitext(file.lower())[1]](f.read())
+
+                LOCK.acquire()
+                ziph.writestr(file, minified_data)
+                LOCK.release()
+        else:
+            LOCK.acquire()
+            ziph.write(file)
+            LOCK.release()
+
+
+MINIFY = {".js": minimize_js, ".css": minimize_css, ".html": minimize_html}
 
 BLOCKLIST = [
     "package.py",
@@ -42,11 +71,13 @@ BLOCKLIST = [
     ".git",
     ".gitattributes",
     ".gitignore",
-    "README.md",
-    "LICENSE"
+    "readme.md",
+    "LICENSE",
 ]
 
 BUILD_FILE = "build/build.zip"
+
+threads = []
 
 
 def zipdir(path: str, ziph: zipfile.ZipFile):
@@ -54,19 +85,19 @@ def zipdir(path: str, ziph: zipfile.ZipFile):
         file = os.path.normpath(os.path.join(path, file)).replace("\\", "/")
 
         if file in BLOCKLIST:
+            LOCK.acquire()
             print("Skipping " + file)
+            LOCK.release()
             continue
 
+        LOCK.acquire()
         print("Adding " + file)
-        if os.path.isdir(file):
-            zipdir(file, ziph)
-        else:
-            if file.lower().endswith(tuple(MINIFY.keys())):
-                with open(file, "rb") as f:
-                    ziph.writestr(file,
-                                  MINIFY[os.path.splitext(file.lower())[1]](f.read()))
-            else:
-                ziph.write(file)
+        LOCK.release()
+
+        thread = threading.Thread(target=add_file_thread, args=(file, ziph))
+        thread.start()
+        threads.append(thread)
+
 
 def getSizeOfDir(path: str):
     size = 0
@@ -81,12 +112,19 @@ if __name__ == "__main__":
     if not os.path.exists("build"):
         os.mkdir("build")
 
-    zipf = zipfile.ZipFile(BUILD_FILE, "w",
-                           zipfile.ZIP_DEFLATED, True, 9)
+    zipf = zipfile.ZipFile(BUILD_FILE, "w", zipfile.ZIP_DEFLATED, True, 9)
     zipdir("./", zipf)
+
+    for thread in threads:
+        thread.join()
+
     zipf.close()
 
     print("=" * 20)
     print("Build complete")
-    print("Build size zipped: " + str(round(os.path.getsize(BUILD_FILE) / 1000000, 3)) + " mb")
+    print(
+        "Build size zipped: "
+        + str(round(os.path.getsize(BUILD_FILE) / 1000000, 3))
+        + " mb"
+    )
     print("Project size: " + str(round(getSizeOfDir(".") / 1000000, 3)) + " mb")
